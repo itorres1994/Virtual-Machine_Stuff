0f0903e47d3d58a7ad4b72fdac487ef5ca4d6b4a
Ian Torres <itorres@umass.edu>
2017-11-08 16:47:15 -0500

    added: pythonrc

---
+++ .pythonrc.py
@@ -0,0 +1,17 @@
+from __future__ import print_function
+import os
+import sys
+import torch
+import numpy as np
+
+def clear():
+	os.system('clear')
+	print('To quit out of python shell: quit()')
+	print('To clear console screen type: clear()\n')
+
+print('To quit out of python shell: quit()')
+print('To clear console screen type: clear()\n')
+print('The following packages have been imported automatically:')
+print('os, sys, torch, numpy (np), print_function\n')
+
+sys.ps1 = '8========D~~~> '
